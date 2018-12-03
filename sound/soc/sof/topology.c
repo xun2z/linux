c27816ab08a295ff0102215a9e53c4089485f7f5
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2018-12-03 14:14:44 -0800

    ASoC: SOF: clean up widget_unload routine
    Move the code that frees the widget's private data outside
    the switch case as it is common for all widgets.
    
    Change-Id: I9358338d7f1b42345dbf9115bb22ce00b760dd15
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- sound/soc/sof/topology.c
+++ sound/soc/sof/topology.c
@@ -1753,15 +1753,12 @@ static int sof_widget_unload(struct snd_soc_component *scomp,
 	switch (swidget->id) {
 	case snd_soc_dapm_dai_in:
 	case snd_soc_dapm_dai_out:
-		dai = swidget->private;
+		dai = (struct snd_sof_dai *)swidget->private;
 
-		/* free dai config */
-		kfree(dai->dai_config);
-
-		/* remove and free dai object */
 		if (dai) {
+			/* free dai config */
+			kfree(dai->dai_config);
 			list_del(&dai->list);
-			kfree(dai);
 		}
 		break;
 	case snd_soc_dapm_pga:
@@ -1773,13 +1770,13 @@ static int sof_widget_unload(struct snd_soc_component *scomp,
 
 		/* free volume table */
 		kfree(scontrol->volume_table);
-
-		/* fallthrough */
+		break;
 	default:
-		/* free private value */
-		kfree(swidget->private);
 		break;
 	}
+
+	/* free private value */
+	kfree(swidget->private);
 
 	/* remove and free swidget object */
 	list_del(&swidget->list);
